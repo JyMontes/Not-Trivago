77ffe8d0e12984cc4ecd37ac7e29a42afcd0e898
Jose Yael Montes Lopez <jymontes13@gmail.com>
2020-02-13 17:41:48 -0600

    Vista para el componente principal

---
+++ src/views/principal.vue
@@ -0,0 +1,16 @@
+<template>
+  <principal />
+</template>
+
+<script>
+import principal from '../components/principal'
+export default {
+  components: {
+    principal
+  }
+}
+</script>
+
+<style>
+
+</style>
