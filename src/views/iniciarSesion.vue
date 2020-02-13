874eef99879e1508498752c49eef06f13a54713d
Jose Yael Montes Lopez <jymontes13@gmail.com>
2020-02-13 17:40:32 -0600

    Vista para el componente de iniciar sesion

---
+++ src/views/iniciarSesion.vue
@@ -0,0 +1,16 @@
+<template>
+  <iniciarSesion />
+</template>
+
+<script>
+import iniciarSesion from '../components/iniciarSesion'
+export default {
+  components: {
+    iniciarSesion
+  }
+}
+</script>
+
+<style>
+
+</style>
