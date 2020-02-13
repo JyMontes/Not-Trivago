79d81ac024243c3152fd750385e06c2efa35f7ef
Jose Yael Montes Lopez <jymontes13@gmail.com>
2020-02-13 17:38:13 -0600

    Vista para el componente agregar hoteles

---
+++ src/views/agregarHoteles.vue
@@ -0,0 +1,16 @@
+<template>
+  <agregarHotel />
+</template>
+
+<script>
+import agregarHotel from '../components/agregarHoteles'
+export default {
+  components: {
+    agregarHotel
+  }
+}
+</script>
+
+<style>
+
+</style>
